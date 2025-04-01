23765b184a16e4f1ffd182776e984be87e7893ea
lagigliaivan <lagigliaivan@gmail.com>
2025-04-01 16:46:15 -0300

    stable version

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,28 +1,26 @@
 import java.util.HashSet;
-import java.util.concurrent.locks.ReentrantLock;
-// Add Word: Add a word to the dictionary.
-// Search Word: Check if a word exists in the dictionary.
-// Auto-Complete: Given a prefix, return a list of words that start with that prefix.
-// Remove Word: Remove a word from the dictionary.
-// Additional items
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 // Create unit tests to showcase every functionality.
 // Assume the dictionary would work in a concurrent environment.
 
 public class Dictionary {
-    private HashSet<String> words;
-    private Lock lock;
+    private final HashSet<String> words;
+    private final Lock lock;
 
     public Dictionary() {
-        words = new HashSet<String>();
+        words = new HashSet<>();
         lock = new ReentrantLock();
     }
 
     public void Add(String word) {
-        lock.lock();
-        words.add(word);
-        lock.unlock();
+        try{
+            lock.lock();
+            words.add(word);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public boolean Contains(String word) {
@@ -32,20 +30,27 @@ public class Dictionary {
     public Dictionary Prefix(String prefix) {
         Dictionary d = new Dictionary();
 
-        lock.lock();
-        for (String word : words) {
-            if (word.contains(prefix)) {
-                d.Add(word);
+        try{ 
+            lock.lock();
+            
+            for (String word : words) {
+                if (word.contains(prefix)) {
+                    d.Add(word);
+                }
             }
+        }   finally {
+            lock.unlock();
         }
-        lock.unlock();
 
         return d;
     }
 
     public void Remove(String word) {
-        lock.lock();
-        words.remove(word);
-        lock.unlock();
+        try {
+            lock.lock();
+            words.remove(word);
+        } finally {
+            lock.unlock();
+        }
     }
 }
